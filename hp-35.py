0615c380e2947d526cf6c2230836a543c1dbf554
ve1dx <82286137+ve1dx@users.noreply.github.com>
2022-09-28 13:52:18 -0300

    Initial commit

--- hp-35.py
+++ hp-35.py
@@ -1,5 +1,36 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+import sys
+
+
 def show_display():
-    
+    numeric_display = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    print(numeric_display)
+
+
+def python_check():
+    version_major = sys.version_info[0]
+    version_minor = sys.version_info[1]
+    version_micro = sys.version_info[2]
+    if not (version_major == 3 and version_minor >= 5):
+        print("Python verson 3.5 or higher required to run wee_trend. This system is running Python version ",
+              version_major, ".", version_minor, ".", version_micro, sep="")
+        print()
+        print("Exiting program.")
+        sys.exit(0)
+
+
+def main():
+    try:
+        python_check()
+        show_display()
+    except KeyboardInterrupt:
+        print()
+        print("Keyboard interrupt by user")
+        print()
+        print()
+
+
+if __name__ == "__main__":
+    main()
